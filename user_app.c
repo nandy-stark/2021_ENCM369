3a7c012b6da55f71814836cf21eb03ce95b5a44d
nandy-stark <67021448+nandy-stark@users.noreply.github.com>
2021-04-09 14:58:58 -0600

    Revert ""
    This reverts commit 43f3c8d73a62c3a048f80d0de74fd3351be014c0.

--- user_app.c
+++ user_app.c
@@ -30,11 +30,22 @@ Global variable definitions with scope across entire project.
 All Global variable names shall start with "G_<type>UserApp1"
 ***********************************************************************************************************************/
 /* New variables */
-volatile u8 G_u8UserAppFlags;                  /*!< @brief Global state flags */
-volatile u8 G_u8UserAppTimePeriodHi;           /*!< @brief Global saved Timer1 high count for ISR */
-volatile u8 G_u8UserAppTimePeriodLo;           /*!< @brief Global saved Timer1 low count for ISR */
+volatile u8 G_u8UserAppFlags;                             /*!< @brief Global state flags */
 
-u8 G_au8UserAppsinTable[] = 
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+/* Existing variables (defined in other files -- should all contain the "extern" keyword) */
+extern volatile u32 G_u32SystemTime1ms;                   /*!< @brief From main.c */
+extern volatile u32 G_u32SystemTime1s;                    /*!< @brief From main.c */
+extern volatile u32 G_u32SystemFlags;                     /*!< @brief From main.c */
+
+
+/***********************************************************************************************************************
+Global variable definitions with scope limited to this local application.
+Variable names shall start with "UserApp_<type>" and be declared as static.
+***********************************************************************************************************************/
+static u8 u8Index=0;
+static u8 UserApp_au8sinTable[] =
 {
 0x80,0x83,0x86,0x89,0x8c,0x8f,0x92,0x95,0x98,0x9b,0x9e,0xa2,0xa5,0xa7,0xaa,0xad,
 0xb0,0xb3,0xb6,0xb9,0xbc,0xbe,0xc1,0xc4,0xc6,0xc9,0xcb,0xce,0xd0,0xd3,0xd5,0xd7,
@@ -51,22 +62,8 @@ u8 G_au8UserAppsinTable[] =
 0x00,0x00,0x00,0x00,0x01,0x01,0x01,0x02,0x02,0x03,0x04,0x05,0x05,0x06,0x07,0x09,
 0x0a,0x0b,0x0c,0x0e,0x0f,0x11,0x12,0x14,0x15,0x17,0x19,0x1b,0x1d,0x1f,0x21,0x23,
 0x25,0x28,0x2a,0x2c,0x2f,0x31,0x34,0x36,0x39,0x3b,0x3e,0x41,0x43,0x46,0x49,0x4c,
-0x4f,0x52,0x55,0x58,0x5a,0x5d,0x61,0x64,0x67,0x6a,0x6d,0x70,0x73,0x76,0x79,0x7c   
+0x4f,0x52,0x55,0x58,0x5a,0x5d,0x61,0x64,0x67,0x6a,0x6d,0x70,0x73,0x76,0x79,0x7c
 };
-
-
-/*--------------------------------------------------------------------------------------------------------------------*/
-/* Existing variables (defined in other files -- should all contain the "extern" keyword) */
-extern volatile u32 G_u32SystemTime1ms;                   /*!< @brief From main.c */
-extern volatile u32 G_u32SystemTime1s;                    /*!< @brief From main.c */
-extern volatile u32 G_u32SystemFlags;                     /*!< @brief From main.c */
-
-
-/***********************************************************************************************************************
-Global variable definitions with scope limited to this local application.
-Variable names shall start with "UserApp_<type>" and be declared as static.
-***********************************************************************************************************************/
-
 
 /**********************************************************************************************************************
 Function Definitions
@@ -75,112 +72,6 @@ Function Definitions
 /*--------------------------------------------------------------------------------------------------------------------*/
 /*! @publicsection */                                                                                            
 /*--------------------------------------------------------------------------------------------------------------------*/
-
-/*!--------------------------------------------------------------------------------------------------------------------
-@fn void TimeXus(u16 u16TimeXus_)
-
-@brief
-Sets up Timer0 to provide an interrupt at u16Microseconds_
-
-Requires:
-- Timer0 configured such that each timer tick is 1 microsecond
-
-Promises:
-- Pre-loads TMR0H:L to clock out desired period
-- TMR0IF cleared
-- Timer0 enabled
-
-*/
-void TimeXus(u16 u16TimeXus_)
-{
-  u16 u16Temp = 65535;
-
-#if 0  
-  /* Handle edge case */
-  if(u16TimeXus_ == 0)
-  {
-      PIR3bits.TMR0IF = 1;
-      return;
-  }
-#endif
-  
-  /* Disable the timer during config */
-  T0CON0bits.EN = 0;
-  
-  /* Preload TMR0H and TMR0L based on u16TimeXus */
-  u16Temp -= u16TimeXus_;
-  TMR0H = (u8)( (u16Temp >> 8) & 0x00FF);
-  TMR0L = (u8)( u16Temp & 0x00FF);
-   
-  /* Clear TMR0IF and enable Timer 0 */
-  PIR3bits.TMR0IF = 0;
-  T0CON0bits.EN = 1;
-  
-} /* end TimeXus() */
-
-
-/*!--------------------------------------------------------------------------------------------------------------------
-@fn void InterruptTimerXus(u16 u16TimeXus_, bool bContinuous_)
-
-@brief
-Maximum 32,767us.  Arguments higher than this will be capped back.
-Sets up Timer1 to provide an interrupt every u16TimeXus_ microseconds.
-This can be configured as a single event, or continuous.
-Note: it would be much better to pass a call-back function
-parameter to this function to register the call-back for the ISR
-to use, but we'll hard-code it for now.
-
-Requires:
-- Timer1 configured such that each timer tick is 0.5 microseconds
-- TMR1_ISR holds code to respond to the interrupt
-- bContinuous_ is true if the timer should run continuously;
-  false if it should run once and stop.
-
-Promises:
-- Pre-loads TMR1H:L to clock out desired period
-- G_u8UserAppTimePeriodHi/Lo updated to save period
-- _U8_CONTINUOUS updated per bContinuous_
-- TMR1IF cleared and interrupt enabled
-- Timer1 enabled
-
-*/
-void InterruptTimerXus(u16 u16TimeXus_, bool bContinuous_)
-{
-  u16 u16Temp;
-
-  /* Disable the timer during config */
-  T1CONbits.ON = 0;
-  
-  /* Correct the input parameter if it's too high */
-  if(u16TimeXus_ > 32767)
-  {
-    u16TimeXus_ = 32767;
-  }
-
-  /* Double the time so it's in us not 0.5us*/
-  u16Temp = u16TimeXus_ << 1;
-   
-  /* Calculate, save, and preload TMR1H and TMR1L based on u16TimeXus_ */
-  u16Temp = 65535 - u16TimeXus_;
-  G_u8UserAppTimePeriodHi = (u8)( (u16Temp >> 8) & 0x00FF);
-  G_u8UserAppTimePeriodLo = (u8)( u16Temp & 0x00FF);
-  TMR1H = G_u8UserAppTimePeriodHi;
-  TMR1L = G_u8UserAppTimePeriodLo;
-  
-  /* Flag continuous mode if required */
-  G_u8UserAppFlags &= ~_U8_CONTINUOUS;
-  if(bContinuous_)
-  {
-    G_u8UserAppFlags |= _U8_CONTINUOUS;
-  }
-  
-  /* Clear the interrupt flag, enable interrupt and enable Timer */
-  PIR3bits.TMR1IF = 0;
-  PIE3bits.TMR1IE = 1;
-  T1CONbits.ON = 1;
-  
-} /* end InterruptTimerXus() */
-
 
 /*--------------------------------------------------------------------------------------------------------------------*/
 /*! @protectedsection */                                                                                            
@@ -198,51 +89,61 @@ Requires:
 - NONE
 
 Promises:
-- Start with RA6:0 low
+- NONE
 
 */
 void UserAppInitialize(void)
 {
-    /* LED initialization */
-    LATA &= 0xC0;
+    T0CON0 = 0x90;
     
-    /* Timer0 control register initialization to turn timer on, asynch mode, 16-bit
-     * Fosc/4, 1:16 prescaler, 1:1 postscaler  */
-    T0CON0 = 0x90; // b'10010000'
-    T0CON1 = 0x54; // b'01010100'
+    T0CON1 = 0x54;
     
-    /* Timer1 initialization:
-     * 1:8 prescale, synced, enabled */
-    T1GCON = 0x00;
-    T1CLK  = 0x01;  
-    T1CON  = 0x31;  // b'00110001'
+    LATA = 0x80;
     
-    // Test call to set frequency
- 
-    InterruptTimerXus(16, true);   
+    DAC1DATL = 0;
+
 } /* end UserAppInitialize() */
 
   
 /*!----------------------------------------------------------------------------------------------------------------------
 @fn void UserAppRun(void)
-
 @brief Application code that runs once per system loop
-
 Requires:
 - 
-
 Promises:
 - 
-
 */
 void UserAppRun(void)
 {
+    DAC1DATL = UserApp_au8sinTable[u8Index += 4];
+} /* end UserAppRun */
+
+
+/*--------------------------------------------------------------------
+void TimeXus(INPUT_PARAMETER_)
+Sets Timer0 to count u16Microseconds_
+Requires:
+- Timer0 configured such that each timer tick is 1 microsecond
+- INPUT_PARAMETER_ is the value in microseconds to time from 1 to 65535
+Promises:
+- Pre-loads TMR0H:L to clock out desired period
+- TMR0IF cleared
+- Timer0 enabled
+*/
+
+void TimeXus(u16 u16usecs)
+{
+    T0CON0 &= 0x7F;
     
-
-  
-} /* end UserAppRun() */
-
-
+    u16 u16Timer = 0xFFFF - u16usecs;
+    
+    TMR0L = u16Timer & 0x00FF;
+    TMR0H = (u8) ((u16Timer & 0xFF00) >> 8);
+    
+    PIR3 &= 0x7F;
+    T0CON0 |= 0x80;
+} 
+/* end TimeXus () */
 
 /*------------------------------------------------------------------------------------------------------------------*/
 /*! @privatesection */                                                                                            
@@ -253,5 +154,4 @@ void UserAppRun(void)
 
 
 /*--------------------------------------------------------------------------------------------------------------------*/
-/* End of File                                                                                                        */
-/*--------------------------------------------------------------------------------------------------------------------*/
+//* End of File                      
