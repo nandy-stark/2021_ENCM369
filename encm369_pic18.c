82e5098a184691b8490e52e5259478bf7d9402fd
nandy-stark <67021448+nandy-stark@users.noreply.github.com>
2021-04-09 14:56:47 -0600

    Revert ""
    This reverts commit 95a4b8ab6edbddbc0dba75f26ab5fb70c186492c.

--- encm369_pic18.c
+++ encm369_pic18.c
@@ -22,7 +22,6 @@ PROTECTED FUNCTIONS
 ***********************************************************************************************************************/
 
 #include "configuration.h"
-#include "pic18f27q43.h"
 
 /***********************************************************************************************************************
 Global variable definitions with scope across entire project.
@@ -37,7 +36,7 @@ All Global variable names shall start with "G_xxBsp"
 /* Existing variables (defined in other files -- should all contain the "extern" keyword) */
 extern volatile u32 G_u32SystemTime1ms;        /*!< @brief From main.c */
 extern volatile u32 G_u32SystemTime1s;         /*!< @brief From main.c */
-extern volatile u8  G_u8SystemFlags;           /*!< @brief From main.c */
+extern volatile u32 G_u32SystemFlags;          /*!< @brief From main.c */
 
 /***********************************************************************************************************************
 Global variable definitions with scope limited to this local application.
@@ -84,53 +83,35 @@ Requires:
 - All configurations must match connected hardware.
 
 Promises:
-- PORTA setup for LED output
+- Output pin for PA31_HEARTBEAT is configured
 
 */
+
 void GpioSetup(void)
 {
-  /* Setup PORTA for all digital output */
-  ANSELA = 0x00;
-  TRISA  = 0x00;
-   
-  /* Configure DAC1 for Vdd and Vss references, on, and RA2 output. */
-  DAC1CON  = 0xA0;
-  DAC1DATL = 0;
- 
+    TRISA = 0x00;
+    ANSELA = 0x00;
+    DAC1CON = 0xA0;
+    DAC1DATL = 0;
+  
 } /* end GpioSetup() */
 
 
 /*!---------------------------------------------------------------------------------------------------------------------
 @fn  void SysTickSetup(void)
-
 @brief Initializes the 1ms and 1s System Ticks off the core timer.
-
 Requires:
 - NVIC is setup and SysTick handler is installed
-
 Promises:
-- Both global system timers are reset Timer0 is configured for 1ms intervals
-
+- Both global system timers are reset and the SysTick core timer is configured for 1ms intervals
 */
 void SysTickSetup(void)
 {
   G_u32SystemTime1ms = 0;      
   G_u32SystemTime1s  = 0;   
   
-  /* Setup Timer2 for 1ms period 
-   * Input clock: Fosc / 4 = 16MHz
-   * Maximum prescaler 128 > 125kHz > 8us period
-   * 125 x 8us = 1ms so use Timer2 match to 124
-   * No postscaler requried */
-  
-  T2PR = 125;             // Match register for 1ms period
-  T2CLKCON = 0x01;        // b'00000001' Fosc/4 input
-  PIR3bits.TMR2IF = 0;    // Make sure interrupt flag is clear to start
-  T2CON = 0xF0;           // b'11110000' Timer on, 1:128 prescale, 1:1 postscaler
-  
-  PIE3bits.TMR2IE = 1;    // Enable Timer2 interrupt: Systick is now running
-  
 } /* end SysTickSetup() */
+
 
 
 
@@ -149,8 +130,6 @@ Promises:
 */
 void SystemSleep(void)
 {    
-  G_u8SystemFlags |= _SYSTEM_SLEEPING;
-  while(G_u8SystemFlags & _SYSTEM_SLEEPING);
   
 } /* end SystemSleep(void) */
 
